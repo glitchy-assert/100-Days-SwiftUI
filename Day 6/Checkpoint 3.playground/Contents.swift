c6b4fceecfb8b1d85fbf83ead48707e7f1b3ea4a
Andrii Berestov <andrii@berestov.dev>
2025-09-21 23:41:19 +0300

    checkpoint 3

---
+++ Day 6/Checkpoint 3.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Cocoa
+
+for i in 1...100 {
+  var result = ""
+    if i.isMultiple(of: 3) {
+        result += "Fizz"
+    }
+    if i.isMultiple(of: 5) {
+        result += "Buzz"
+    }
+    if result.isEmpty {
+        result = "\(i)"
+    }
+    print(result)
+}
